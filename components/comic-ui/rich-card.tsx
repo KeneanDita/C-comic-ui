c9c07798f471acb61e011b8204a50ce202b3090b
Kenean Dita <kenean.dita@haramaya.edu.et>
2026-04-15 19:00:14 +0300

    fix: omit title from RichCardProps interface to prevent prop conflicts

--- components/comic-ui/rich-card.tsx
+++ components/comic-ui/rich-card.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/comic-ui/button"
 
-export interface RichCardProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface RichCardProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "title"> {
   imageSrc?: string;
   badgeText?: string;
   badgeIcon?: React.ReactNode;
